01ef4b5639f2cf9b4efd321a011607f1d09ca020
hortegag <hog_andy@hotmail.com>
2022-02-08 21:48:55 -0500

    Added project files

---
+++ demo/src/test/java/com/kata/budgetplanner/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.budgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
